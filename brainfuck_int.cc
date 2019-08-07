41e9a34af73c0f8c6527428a5c4e1bad0817797c
Guillem Ramírez Miranda <guillem.ramirez.miranda@est.fib.upc.edu>
2019-08-07 17:43:07 +0200

    Update brainfuck_int.cc

--- brainfuck_int.cc
+++ brainfuck_int.cc
@@ -4,7 +4,6 @@
 using namespace std;
 
 const int MAX_MEMORY = 30000; //#The number of 8 bit memory cells that the original brainfuck had  
-//you should show how brainfuck works, jej
 
 int main(){
     vector<char> memory(MAX_MEMORY, 0);
